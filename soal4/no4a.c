ffe4c5b8b2aeae6e7f413797252a8296d7ba9cbb
herripurba <58382561+herripurba@users.noreply.github.com>
2020-04-10 20:18:19 +0700

    Update no4a.c

--- soal4/no4a.c
+++ soal4/no4a.c
@@ -8,29 +8,33 @@
 #include<sys/ipc.h>
 #include<sys/shm.h>
 
-#define MAX_THREAD 1
-//#define SHFILE 100
+#define MAX_THREAD 4
 
 int matrikA[4][2] ={
-    {2,3},
-    {3,1},
-    {2,2},
-    {1,5}
+    {2,8},
+    {8,2},
+    {1,3},
+    {2,5}
 };
 int matrikB[2][5]= {
     {6,0,5,5,4},
     {0,4,3,1,3}
 };
 int matrikC[4][5];
+int iter =0;
+
 void* perkalianMatriks(void *arg)
 {
-    for(int i = 0 ; i < 4; i++){
+    
+    for (int i = iter ; i < (iter + 1) ; i++){
         for(int j = 0; j < 5; j++){
             for(int k =0; k < 2; k++){
                 matrikC[i][j] += matrikA[i][k] * matrikB[k][j];
+         
             }
-        }
+        }   
     }
+    iter++;
 }
 
 int main(void)
